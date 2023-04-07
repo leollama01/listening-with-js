e3f1cc19e314cfde9c2562dca2200b36e1773c49
leollama01 <leollama01@gmail.com>
2023-04-06 23:16:06 -0300

    Developing

--- project/script.js
+++ project/script.js
@@ -1,1 +1,14 @@
-console.log($)
+$(function() {
+    
+    let btnListen = $('#btn-listen');
+    let btnSend = $('#btn-send');
+
+    btnListen.click(function() {
+        console.log('listening')
+    })
+
+    btnSend.click(function() {
+        console.log('send text')
+    })
+
+})
