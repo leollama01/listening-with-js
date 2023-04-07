add77de6d96bae6f64fdaf8dc01b1429d6fede99
leollama01 <leollama01@gmail.com>
2023-04-07 00:12:57 -0300

    Listening voice

--- README.md
+++ README.md
@@ -2,5 +2,10 @@
 
 #### Using the Web Speech API to listen voice through the web browser.  
 
+#### References
 * [Web Speech API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API)  
+* [Perform Speech Recognition in Your Javascript Applications](https://betterprogramming.pub/perform-speech-recognition-in-your-javascript-applications-91367b0d0)
+* [Speech Recognition Using the Web Speech API in JavaScript](https://www.section.io/engineering-education/speech-recognition-in-javascript/)
+
+#### Browser used for development: *Google Chrome*
 
--- project/script.js
+++ project/script.js
@@ -4,11 +4,55 @@ $(function() {
     let btnSend = $('#btn-send');
 
     btnListen.click(function() {
-        console.log('listening')
+        if ($(this).attr('listen') == 'true') {
+            $(this).text('Listen');
+            $(this).css('background-color', '');
+            $(this).attr('listen', 'false');
+            recognition.stop();
+
+        } else {
+            $(this).text('Stop Listening');
+            $(this).css('background-color', 'red');
+            $(this).attr('listen', 'true');
+            recognition.start();
+        }
     })
 
     btnSend.click(function() {
-        console.log('send text')
+        sendRequestChatGpt()
     })
 
+    let chat = $('#chat')
+    const recognition = new webkitSpeechRecognition();
+    recognition.continuous = true;
+    recognition.interimResults = true;
+    recognition.onresult = function (event) {
+        let result = '';
+        for (let i = event.resultIndex; i < event.results.length; i++) {
+            result += event.results[i][0].transcript;
+        }
+
+        console.log(result)
+        
+        /**
+         * Just for test
+         */
+        // if ((result.toUpperCase()).includes('SEND') && (result.toUpperCase()).includes('MESSAGE')) {
+        //     $('#btn-send').click()
+        // }
+
+        chat.text('')
+        chat.append(` \
+            <p author="voice"> \
+                ${result} \
+            </p> \
+        `)
+    };
+
 })
+
+
+function sendRequestChatGpt() {
+    console.log('send request')
+}
+
